52ace166c5a3e4f654e49a38cc8fa72d8218f1c6
Hanne <hannehoitzing@gmail.com>
2025-05-05 09:21:35 +0100

    Join data

--- src/kindra/kindle.py
+++ src/kindra/kindle.py
@@ -14,45 +14,44 @@ def query_db(query: str) -> pl.DataFrame:
     return pl.read_database(query=query, connection=conn)
 
 
-def dump_data(book: str):
-    """dump the vocab.db data, create a parquet per book
+def dump_data(book_title: str):
+    """
+        dump the vocab.db data, create a parquet per book
 
     """
 
     # query vocab.db file
-    info = query_db("select * from BOOK_INFO")
+    info = query_db("select * from BOOK_INFO"
+        ).select(["id", "title"]
+        ).filter(col("title") == book_title
+        )
+        
     words = query_db("select * from WORDS")
-    lookup = query_db("select * from LOOKUPS")
-
-    vocab = (info
-    .join(lookup, left_on=["id"], right_on=["book_key"])
-    .drop(["id_right", "asin", "guid", "dict_key", "pos", "authors", "id"])
-    .join(words, left_on=["word_key"], right_on=["id"])
-    .drop(["category", "lang_right", "timestamp_right", "profileid"])
-    .unique(["word", "title"])  # could be same word with different context... but costs more money. 
-    .with_columns(pl.struct(["word", "usage"]).map_elements(lambda x: utils.add_boldface(x["word"], x["usage"])).alias("usage"))
-    )
-
-    titles = vocab["title"].unique()
-    all_titles = []
-
-    for tit in titles:
-
-        book_vocab = (vocab
-            .filter(col("title") == tit)
-            .sort("timestamp")
+    
+    lookup = query_db("select * from LOOKUPS"
+        ).select(["book_key", "timestamp", "usage", "word_key"]
         )
 
-        valid_title = utils.get_valid_filename(tit)  # remove spaces, weird punctuations, etc.. 
-        
-        Path(DUMPED_BOOK_FOLDER).mkdir(parents=True, exist_ok=True)
-        book_vocab.write_parquet(file=DUMPED_BOOK_FILE_NAME.format(book_name=valid_title))
+    vocab = (info
+    .join(lookup, left_on="id", right_on="book_key")
+    .join(words, left_on=["word_key"], right_on=["id"])
+    .unique(["word", "title"])  # could be same word with different context... but costs more money. 
+    .with_columns(pl.struct(["word", "usage"]).map_elements(lambda x: utils.add_boldface(x["word"], x["usage"])).alias("usage"))
+    ).select(
+        ["title", "usage", "timestamp", "stem", "lang"]
+    ).sort(
+        "timestamp", descending=False
+    )
 
-        all_titles.append(valid_title)
+    valid_title = utils.get_valid_filename(book_title)  # remove spaces, weird punctuations, etc.. 
+    
+    Path(DUMPED_BOOK_FOLDER).mkdir(parents=True, exist_ok=True)
+    vocab.write_parquet(file=DUMPED_BOOK_FILE_NAME.format(book_name=valid_title))
 
-    # save the titles that are the names of the parquet files. Use later to read them back in
-    with open(f"{META_DATA_FOLDER}/{TITLES_FILE_NAME}.json", 'w') as f:
-        json.dump(all_titles, f)
+
+    # # save the titles that are the names of the parquet files. Use later to read them back in
+    # with open(f"{META_DATA_FOLDER}/{TITLES_FILE_NAME}.json", 'w') as f:
+    #     json.dump(all_titles, f)
 
 
 if __name__ == "__main__":
